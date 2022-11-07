5687436ee3fd30723d3dc96a65dcb7ffe343785b
Yli-Juuti <niko.yli-juuti@aalto.fi>
2022-11-07 17:38:55 +0200

    aa

--- part3/index.js
+++ part3/index.js
@@ -3,102 +3,103 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const app = express()
-const Person = require('./models/person') 
+const Person = require('./models/person')
 app.use(express.static('build'))
 app.use(cors())
 app.use(express.json())
 
 app.use(morgan((tokens, req, res) => {
-  return [
-    tokens.method(req, res),
-    tokens.url(req, res),
-    tokens.status(req, res),
-    tokens.res(req, res, 'content-length'), '-',
-    tokens['response-time'](req, res), 'ms',
-    JSON.stringify(req.body)
-  ].join(' ')
+    return [
+        tokens.method(req, res),
+        tokens.url(req, res),
+        tokens.status(req, res),
+        tokens.res(req, res, 'content-length'), '-',
+        tokens['response-time'](req, res), 'ms',
+        JSON.stringify(req.body)
+    ].join(' ')
 }))
 
 app.get('/info', (req, res) => {
-  Person.find({}).then(persons => {
-    res.send(`<p>Phonebook has info for ${persons.length} people </p> ${Date()}`)})
+    Person.find({}).then(persons => {
+        res.send(`<p>Phonebook has info for ${persons.length} people </p> ${Date()}`)})
 })
 
 app.post('/api/persons', (req, res, next) => {
-  const body = req.body
+    const body = req.body
 
-  if (body.content === undefined) {
-    return res.status(400).json({ error: 'content missing' })
-  }
-  const person = new Person({
-    name: body.name,
-    number: body.number
-  })
-  person.save()
-  .then(savedPerson => {
-    res.json(savedPerson)
-  })
-  .catch(error => next(error) )
+    const person = new Person({
+        name: body.name,
+        number: body.number
+    })
+    person.save()
+        .then(savedPerson => {
+            res.json(savedPerson)
+        })
+        .catch(error => next(error) )
 })
 
 app.get('/api/persons', (req, res) => {
-  Person.find({}).then(persons => {
-    res.json(persons)
-  })
-}) 
+    Person.find({}).then(persons => {
+        res.json(persons)
+    })
+})
 
 app.delete('/api/persons/:id', (req, res, next) => {
-  Person.findByIdAndRemove(req.params.id)
-  .then(() => {
-    res.status(204).end()
-  })
-  .catch(error => next(error) )
+    Person.findByIdAndRemove(req.params.id)
+        .then(() => {
+            res.status(204).end()
+        })
+        .catch(error => next(error) )
 })
 
 app.get('/api/persons/:id', (req, res, next) => {
-  Person.findById(req.params.id)
-    .then(person => {
-      if (person) {
-        res.json(person)
-      } else {
-        res.status(404).end()
-      }
-    })
-    .catch(error => {
-      next(error)
-    })
+    Person.findById(req.params.id)
+        .then(person => {
+            if (person) {
+                res.json(person)
+            } else {
+                res.status(404).end()
+            }
+        })
+        .catch(error => {
+            next(error)
+        })
 })
 
 app.put('/api/persons/:id', (req, res, next) => {
-  const body = req.body
-  const person = {
-    name: body.name,
-    number: body.number,
-  }
-Person.findByIdAndUpdate(req.params.id, person, { new: true })
-  .then(updated => {
-    res.json(updated)
-  })
-  .catch(error => next(error))
+    const body = req.body
+    const person = {
+        name: body.name,
+        number: body.number,
+    }
+    Person.findByIdAndUpdate(req.params.id, person, { new: true })
+        .then(updated => {
+            res.json(updated)
+        })
+        .catch(error => next(error))
 })
 
-const unknownEndpoint = (req, res) => {
-  res.status(404).send({ error: 'unknown endpoint' })
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
 }
 
 app.use(unknownEndpoint)
 
-const errorHandler = (error, req, res, next) => {
-  console.error(error.message)
-  if (error.name === 'CastError') {
-    return res.status(400).send({ error: 'malformatted id' })
-  }
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
 
-  next(error)
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
+    }
+
+    next(error)
 }
 
 app.use(errorHandler)
 
+// eslint-disable-next-line no-undef
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
--- part3/models/person.js
+++ part3/models/person.js
@@ -1,34 +1,43 @@
 const mongoose = require('mongoose')
 
+// eslint-disable-next-line no-undef
 const url = process.env.MONGODB_URI
 
 console.log('connecting to', url)
 mongoose.connect(url)
-  .then(result => {
-        console.log('connected to MongoDB')  
-    })  
-    .catch((error) => {    
-        console.log('error connecting to MongoDB:', error.message)  
+    .then(() => {
+        console.log('connected to MongoDB')
+    })
+    .catch((error) => {
+        console.log('error connecting to MongoDB:', error.message)
     })
 
 const personSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
+        minlength: 3,
         unique: true
-      },
-      number: {
+    },
+    number: {
         type: String,
-        required: true
-      }
+        required: true,
+        validate: {
+            validator: function(v) {
+                return (/d{3}-/.test(v) || /d{2}-/.test(v))
+            },
+            message: props => `${props.value} is not a valid phone number!`
+        },
+        minlength: 8
+    }
 })
 
 personSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
 })
 
 module.exports = mongoose.model('Person', personSchema)
