ffa2fd126c707a6e0f0860c2ad543a28fff05679
Yli-Juuti <niko.yli-juuti@aalto.fi>
2023-03-28 12:52:01 +0300

    part3 checks

--- part3/mongo.js
+++ part3/mongo.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose')
 
 if (process.argv.length<3) {
-  console.log('give password as argument')
-  process.exit(1)
+    console.log('give password as argument')
+    process.exit(1)
 }
 
 const password = process.argv[2]
@@ -11,32 +11,32 @@ const url = `mongodb+srv://fullstack:${password}@cluster0.a8livaf.mongodb.net/?r
 mongoose.connect(url)
 
 const personSchema = new mongoose.Schema({
-  id: Number,
-  name: String,
-  number: String,
+    id: Number,
+    name: String,
+    number: String,
 })
 
 const Person = mongoose.model('Person', personSchema)
 
 if (process.argv.length === 3){
-  Person.find({}).then(result => {
-    result.forEach(person => {
-      console.log(person.name, person.number)
+    Person.find({}).then(result => {
+        result.forEach(person => {
+            console.log(person.name, person.number)
+        })
+        mongoose.connection.close()
     })
-    mongoose.connection.close()
-  })
 }
 
 if (process.argv.length > 3){
-  const person = new Person({
-    id: Math.floor(Math.random() *100),
-    name: process.argv[3],
-    number: process.argv[4],
-  })
-  person.save().then(result => {
-    console.log('person saved!')
-    mongoose.connection.close()
-  })
+    const person = new Person({
+        id: Math.floor(Math.random() *100),
+        name: process.argv[3],
+        number: process.argv[4],
+    })
+    person.save().then(result => {
+        result.foreach(console.log('person saved!'))
+        mongoose.connection.close()
+    })
 }
 
 
